1797531c18e8cb1272bd5cf168a222406447a2b3
Asrul Kadir <asrulkadir.asus4@gmail.com>
2024-09-14 18:19:43 +0800

    fix validation name class

--- src/modules/class/class.validation.ts
+++ src/modules/class/class.validation.ts
@@ -3,14 +3,14 @@ import { z, ZodType } from 'zod';
 export class ClassValidation {
   static readonly CREATE: ZodType = z.object({
     clientId: z.string().uuid(),
-    name: z.string().min(1).max(100),
+    name: z.string().min(1).max(50),
     homeroomTeacherId: z.string().uuid().optional(),
   });
 
   static readonly UPDATE: ZodType = z.object({
     id: z.string().uuid(),
     clientId: z.string().uuid(),
-    name: z.string().min(1).max(100).optional(),
+    name: z.string().min(1).max(50).optional(),
     homeroomTeacherId: z.string().uuid().optional(),
   });
 
