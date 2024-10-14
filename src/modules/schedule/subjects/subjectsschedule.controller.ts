a0a58628dcc8f2719d29ab925d6ad1fd4d540d23
Asrul Kadir <asrulkadir.asus4@gmail.com>
2024-10-14 21:39:34 +0800

    update delete subject schedule

--- src/modules/schedule/subjects/subjectsschedule.controller.ts
+++ src/modules/schedule/subjects/subjectsschedule.controller.ts
@@ -2,7 +2,15 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { SubjectsScheduleService } from './subjectsschedule.service';
 import { Auth } from 'src/common/decorator/auth.decorator';
 import { UserAuth } from 'src/models/auth.model';
@@ -10,13 +18,17 @@ import { WebResponse } from 'src/models/web.model';
 import {
   CreateSubjectsScheduleRequest,
   SubjectsScheduleResponse,
+  UpdateSubjectsScheduleRequest,
 } from 'src/models/schedule.model';
+import { Roles } from 'src/common/decorator/roles.decorator';
+import { EUserRole } from 'src/utils/enum';
 
 @Controller('api/subjects-schedule')
 export class SubjectsScheduleController {
   constructor(private subjectsSchedule: SubjectsScheduleService) {}
 
   @Post()
+  @Roles(EUserRole.SuperAdmin, EUserRole.Admin)
   async createSubjectsSchedule(
     @Auth() user: UserAuth,
     @Body() request: CreateSubjectsScheduleRequest,
@@ -44,6 +56,62 @@ export class SubjectsScheduleController {
       statusCode: 200,
       status: 'success',
       message: 'Subjects schedule found',
+      data: result,
+    };
+  }
+
+  @Get(':id')
+  async getSubjectsScheduleById(
+    @Auth() user: UserAuth,
+    @Param('id') id: string,
+  ): Promise<WebResponse<SubjectsScheduleResponse>> {
+    const result = await this.subjectsSchedule.getSubjectsScheduleById(
+      user,
+      id,
+    );
+    return {
+      statusCode: 200,
+      status: 'success',
+      message: 'Subjects schedule found',
+      data: result,
+    };
+  }
+
+  @Put(':id')
+  @Roles(EUserRole.SuperAdmin, EUserRole.Admin)
+  async updateSubjectsSchedule(
+    @Auth() user: UserAuth,
+    @Param('id') id: string,
+    @Body() request: CreateSubjectsScheduleRequest,
+  ): Promise<WebResponse<SubjectsScheduleResponse>> {
+    const updateRequest: UpdateSubjectsScheduleRequest = {
+      ...request,
+      clientId: user.clientId,
+      id: id,
+    };
+    const result = await this.subjectsSchedule.updateSubjectsSchedule(
+      id,
+      updateRequest,
+    );
+    return {
+      statusCode: 200,
+      status: 'success',
+      message: 'Subjects schedule updated',
+      data: result,
+    };
+  }
+
+  @Delete(':id')
+  @Roles(EUserRole.SuperAdmin, EUserRole.Admin)
+  async deleteSubjectsSchedule(
+    @Auth() user: UserAuth,
+    @Param('id') id: string,
+  ): Promise<WebResponse<SubjectsScheduleResponse>> {
+    const result = await this.subjectsSchedule.deleteSubjectsSchedule(user, id);
+    return {
+      statusCode: 200,
+      status: 'success',
+      message: 'Subjects schedule deleted',
       data: result,
     };
   }
--- src/modules/schedule/subjects/subjectsschedule.service.ts
+++ src/modules/schedule/subjects/subjectsschedule.service.ts
@@ -15,6 +15,7 @@ import { ValidationService } from 'src/common/service/validation.service';
 import {
   CreateSubjectsScheduleRequest,
   SubjectsScheduleResponse,
+  UpdateSubjectsScheduleRequest,
 } from 'src/models/schedule.model';
 import { SubjectsScheduleValidation } from './subjectsschedule.validation';
 import { UserAuth } from 'src/models/auth.model';
@@ -39,20 +40,38 @@ export class SubjectsScheduleService {
         request,
       );
 
-    // check if subjects schedule already exists
-    const subjectsScheduleExists =
+    // check if subjects schedule is not overlapping
+    const subjectsScheduleOverlapping =
       await this.prismaService.subjectsSchedule.findFirst({
         where: {
           subjectId: createRequest.subjectId,
           classId: createRequest.classId,
           clientId: createRequest.clientId,
           day: createRequest.day,
+          OR: [
+            {
+              startTime: {
+                lte: createRequest.startTime,
+              },
+              endTime: {
+                gte: createRequest.startTime,
+              },
+            },
+            {
+              startTime: {
+                lte: createRequest.endTime,
+              },
+              endTime: {
+                gte: createRequest.endTime,
+              },
+            },
+          ],
         },
       });
 
-    if (subjectsScheduleExists) {
+    if (subjectsScheduleOverlapping) {
       throw new HttpException(
-        'Subjects schedule already exists',
+        'Cannot create subjects schedule, overlapping schedule exists',
         HttpStatus.BAD_REQUEST,
       );
     }
@@ -91,15 +110,7 @@ export class SubjectsScheduleService {
       },
     });
 
-    return {
-      id: subjectsSchedule.id,
-      subject: subjectsSchedule.subject,
-      class: subjectsSchedule.class,
-      day: subjectsSchedule.day,
-      startTime: subjectsSchedule.startTime,
-      endTime: subjectsSchedule.endTime,
-      academicCalendar: subjectsSchedule.academicCalendar,
-    };
+    return subjectsSchedule;
   }
 
   async getSubjectsSchedule(
@@ -146,5 +157,213 @@ export class SubjectsScheduleService {
     );
 
     return subjectsSchedule;
+  }
+
+  async getSubjectsScheduleById(
+    user: UserAuth,
+    id: string,
+  ): Promise<SubjectsScheduleResponse> {
+    this.logger.debug(`getSubjectsScheduleById: id=${id}`);
+    const subjectsSchedule =
+      await this.prismaService.subjectsSchedule.findUnique({
+        where: {
+          clientId: user.clientId,
+          id,
+        },
+        include: {
+          subject: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          class: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          academicCalendar: {
+            select: {
+              id: true,
+              name: true,
+              startTime: true,
+              endTime: true,
+            },
+          },
+          teacher: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
+
+    return subjectsSchedule;
+  }
+
+  async updateSubjectsSchedule(
+    id: string,
+    request: UpdateSubjectsScheduleRequest,
+  ): Promise<SubjectsScheduleResponse> {
+    this.logger.debug(
+      `updateSubjectsSchedule: id=${id}, request=${JSON.stringify(request)}`,
+    );
+
+    const newRequest = { ...request, id };
+    const updateRequest: CreateSubjectsScheduleRequest =
+      this.validationService.validate(
+        SubjectsScheduleValidation.UPDATE,
+        newRequest,
+      );
+
+    // check if subjects schedule exists
+    const subjectsScheduleExists =
+      await this.prismaService.subjectsSchedule.findUnique({
+        where: {
+          clientId: updateRequest.clientId,
+          id,
+        },
+      });
+
+    if (!subjectsScheduleExists) {
+      throw new HttpException(
+        'Subjects schedule not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    // check if subjects schedule is not overlapping
+    const subjectsScheduleOverlapping =
+      await this.prismaService.subjectsSchedule.findFirst({
+        where: {
+          subjectId: updateRequest.subjectId,
+          classId: updateRequest.classId,
+          clientId: updateRequest.clientId,
+          day: updateRequest.day,
+          OR: [
+            {
+              startTime: {
+                lte: updateRequest.startTime,
+              },
+              endTime: {
+                gte: updateRequest.startTime,
+              },
+            },
+            {
+              startTime: {
+                lte: updateRequest.endTime,
+              },
+              endTime: {
+                gte: updateRequest.endTime,
+              },
+            },
+          ],
+          NOT: {
+            id,
+          },
+        },
+      });
+
+    if (subjectsScheduleOverlapping) {
+      throw new HttpException(
+        'Cannot update subjects schedule, overlapping schedule exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const subjectsSchedule = await this.prismaService.subjectsSchedule.update({
+      where: {
+        clientId: updateRequest.clientId,
+        id,
+      },
+      data: updateRequest,
+      include: {
+        subject: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        class: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        academicCalendar: {
+          select: {
+            id: true,
+            name: true,
+            startTime: true,
+            endTime: true,
+          },
+        },
+        teacher: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    return subjectsSchedule;
+  }
+
+  async deleteSubjectsSchedule(user: UserAuth, id: string) {
+    this.logger.debug(`deleteSubjectsSchedule: id=${id}`);
+    const subjectsSchedule = this.prismaService.subjectsSchedule.findUnique({
+      where: {
+        clientId: user.clientId,
+        id,
+      },
+    });
+
+    if (!subjectsSchedule) {
+      throw new HttpException(
+        'Subjects schedule not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const subjectsScheduleDeleted =
+      await this.prismaService.subjectsSchedule.delete({
+        where: {
+          clientId: user.clientId,
+          id,
+        },
+        include: {
+          subject: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          class: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          academicCalendar: {
+            select: {
+              id: true,
+              name: true,
+              startTime: true,
+              endTime: true,
+            },
+          },
+          teacher: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
+
+    return subjectsScheduleDeleted;
   }
 }
